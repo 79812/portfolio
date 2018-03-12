45376be6006fa4ce18e0130a29dbade5b06521a4
Unknown <79812@rocteraa-student.nl>
2018-03-12 09:11:28 +0100

    created index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Positionering">
+	<link <?php	include("inc/favicon.php") ?> 
+	<link rel="stylesheet" type="text/css" href="css/style_homepage.css">
+	<title>Gameworld</title>
+</head>
+<body>
+
+
+
+
+	
+</body>
+</html>
